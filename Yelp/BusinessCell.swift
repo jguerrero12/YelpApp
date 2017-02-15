8fef9ef945b035775da5ffc45bedf91e1d1fa823
Jose A. Guerrero <jose3292@gmail.com>
2017-02-15 01:04:48 -0500

    added required stories
    - user can see the list of Thai restaurants and their info
    - user can search the loaded list of restaurants using navigation bar
    search bar.
    - added red navigation bar to match yelp’s color theme
    - UI elements are auto layout’d (stays the same when device is rotated)

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,54 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Jose Guerrero on 2/13/17.
+//  Copyright © 2017 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var reviewsCountLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var categoriesLabel: UILabel!
+    
+    var business: Business! {
+        didSet {
+            nameLabel.text = business.name
+            thumbImageView.setImageWith(business.imageURL!)
+            categoriesLabel.text = business.categories
+            addressLabel.text = business.address
+            reviewsCountLabel.text = "\(business.reviewCount!) Reviews"
+            ratingImageView.setImageWith(business.ratingImageURL!)
+            distanceLabel.text = business.distance
+            print(business.imageURL!)
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        thumbImageView.layer.cornerRadius = 5
+        thumbImageView.clipsToBounds = true
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,12 +8,40 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController {
+class BusinessesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating,
+ UIScrollViewDelegate {
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    var isMoreDataLoading = false
     var businesses: [Business]!
+    var filteredData: [Business]!
+    var searchController: UISearchController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 105
+        
+        // Initializing with searchResultsController set to nil means that
+        // searchController will use this view controller to display the search results
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        
+        // Prevent dimming of current view controller when searching
+        searchController.dimsBackgroundDuringPresentation = false
+        
+        searchController.searchBar.sizeToFit()
+        navigationItem.titleView = searchController.searchBar
+        
+        // Prevent NavigationBar from hidding when searching, since search bar is in NavigationBar
+        searchController.hidesNavigationBarDuringPresentation = false
+        
+        // Sets this view controller as presenting view controller for the search interface
+        definesPresentationContext = true
         
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
             
@@ -24,6 +52,8 @@ class BusinessesViewController: UIViewController {
                     print(business.address!)
                 }
             }
+            self.filteredData = businesses
+            self.tableView.reloadData()
             
             }
         )
@@ -39,6 +69,55 @@ class BusinessesViewController: UIViewController {
          }
          */
         
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
+        cell.business = filteredData[indexPath.row]
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if filteredData != nil {
+            return filteredData!.count
+        }
+        else{
+            return 0
+        }
+    }
+    
+    func updateSearchResults(for: UISearchController) {
+        if let searchText = searchController.searchBar.text {
+            filteredData = searchText.isEmpty ? businesses : businesses.filter({(aBusiness: Business) -> Bool in
+                // If dataItem matches the searchText, return true to include it
+                let title = aBusiness.name
+                return title!.range(of: searchText, options: .caseInsensitive) != nil
+            })
+            tableView.reloadData()
+        }
+    }
+    
+    func loadMoreData() {
+        
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if (!isMoreDataLoading) {
+            
+            // Calculate the position of one screen length before the bottom of the results
+            let scrollViewContentHeight = tableView.contentSize.height
+            let scrollOffsetThreshold = scrollViewContentHeight - tableView.bounds.size.height
+            
+            // When the user has scrolled past the threshold, start requesting
+            if(scrollView.contentOffset.y > scrollOffsetThreshold && tableView.isDragging) {
+                isMoreDataLoading = true
+                
+                // load more data
+                loadMoreData()
+                
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
